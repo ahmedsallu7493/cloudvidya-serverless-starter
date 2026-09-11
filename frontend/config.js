6670030cf76bae8a3db5fd1fae039f78f7276efa
Ahmed Sallu <sallua543@gmail.com>
2026-09-11 12:25:25 +0530

    Fix CampusFix API URL

--- frontend/config.js
+++ frontend/config.js
@@ -1,3 +1,3 @@
-window.API_BASE = " https://ij0acr7tz0.execute-api.ap-south-1.amazonaws.com/Prod";
+window.API_BASE = "https://ij0acr7tz0.execute-api.ap-south-1.amazonaws.com/Prod";
 window.APP_NAME = "CampusFix";
 window.APP_THEME_COLOR = "#38bdf8";
